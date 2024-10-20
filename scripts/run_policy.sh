ce3122bb1b2fedef164e703567913e5af445a53d
Himanshu Gaurav Singh <55017415+hgaurav2k@users.noreply.github.com>
2024-10-20 10:41:15 -0700

    Update run_policy.sh

--- scripts/run_policy.sh
+++ scripts/run_policy.sh
@@ -1,10 +1,10 @@
 POLICY=$1 #"outputs/AllegroXarmGrasping_scratch_vel_control/2024-05-29_00-49/stage1_nn/ep_41700_step_1708M_reward_1876.28.pth"
 cmd="python scripts/finetune.py   num_gpus=1 \
     task=AllegroXarmNew test=True headless=False \
-    task.env.useOldActionSpace=True \
+    task.env.useOldActionSpace=False \
     checkpoint=$POLICY  \
     train.algo=PPOTransformer \
-    wandb_activate=False  wandb_name=AllegroXarmGrasping_Scratch \
+    wandb_activate=False  wandb_name=AllegroXarmGrasping_Finetuned \
     pipeline=gpu  rl_device=cuda:0  sim_device=cuda:0 \
     train.ppo.minibatch_size=16 num_envs=16 \
     task.env.episodeLength=600 \
