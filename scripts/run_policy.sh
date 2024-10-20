a1c53d8690ee350bf51940c699489284caa077c4
Himanshu Gaurav Singh <55017415+hgaurav2k@users.noreply.github.com>
2024-10-20 10:42:44 -0700

    Update run_policy.sh

--- scripts/run_policy.sh
+++ scripts/run_policy.sh
@@ -1,7 +1,6 @@
 POLICY=$1 #"outputs/AllegroXarmGrasping_scratch_vel_control/2024-05-29_00-49/stage1_nn/ep_41700_step_1708M_reward_1876.28.pth"
 cmd="python scripts/finetune.py   num_gpus=1 \
     task=AllegroXarmNew test=True headless=False \
-    task.env.useOldActionSpace=False \
     checkpoint=$POLICY  \
     train.algo=PPOTransformer \
     wandb_activate=False  wandb_name=AllegroXarmGrasping_Finetuned \
